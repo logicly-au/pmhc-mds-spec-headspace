65d2978861e44ea4ab9feb50ef11298aec8e7b47
Jenni Bennett <jrbenn@gmail.com>
2024-02-06 14:19:35 +1100

    PDF file now builds!

--- doc/conf.py
+++ doc/conf.py
@@ -122,8 +122,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
+
 # Toc options
-'titles_only': True
+#'titles_only': True
+    'analytics_id': 'G-QZ57BJ4BS9',
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -149,6 +151,12 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
